9bc50639d62b34d43cc0e00523f5fb80e76a9956
gersonmatos <matosgerson661@gmail.com>
2024-12-17 11:13:20 -0500

    quienes somos

--- src/views/QuienesSomos.tsx
+++ src/views/QuienesSomos.tsx
@@ -3,15 +3,36 @@ import styles from "./QuienesSomos.module.css";
 
 const QuienesSomos = () => {
   return (
-    <>
-      <Layout>
-        <div className={styles["quienes-somos-container"]}>
-          <h1>Quiénes Somos</h1>
-          <div className={styles["imagen-footer"]}></div>
-          <div className={styles["imagen-footer"]}></div>
+    <Layout>
+      <div className={styles["quienes-somos-container"]}>
+        <h1 className={styles["titulo-principal"]}>Quiénes Somos</h1>
+        <div className={styles["mision-container"]}>
+          <div className={styles["imagen-container"]}></div>
+          <div className={styles["text-container"]}>
+            <h2 className={styles["titulo"]}>Misión</h2>
+            <p className={styles["texto"]}>
+              Contribuir con la producción y difusión de las obras jurídicas que
+              ayudan en el fortalecimiento y desarrollo del conocimiento de
+              nuestra sociedad inmersa en el ámbito jurídico.
+            </p>
+          </div>
         </div>
-      </Layout>
-    </>
+        <div className={styles["vision-container"]}>
+          <div className={styles["imagen-container"]}></div>
+          <div className={styles["text-container"]}>
+            <h2 className={styles["titulo"]}>Visión</h2>
+            <p className={styles["texto"]}>
+              Convertirnos en una editorial referente en la producción, edición
+              y distribución de obras de gran calidad, a través del trabajo
+              conjunto de los mejores autores y colaboradores, con el fin de que
+              nuestro público pueda disfrutar de la lectura. Buscando la
+              excelencia en todo lo que hacemos, desde el momento de la creación
+              hasta su edición y distribución.
+            </p>
+          </div>
+        </div>
+      </div>
+    </Layout>
   );
 };
 
